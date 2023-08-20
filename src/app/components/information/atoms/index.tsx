9d4263181613c27d3467bf3ffdae9555b6394e8c
watanabe0055 <mailok1212+github@gmail.com>
2023-08-21 00:37:47 +0900

    fix: スタイル修正

--- src/app/components/information/atoms/index.tsx
+++ src/app/components/information/atoms/index.tsx
@@ -1,46 +1,26 @@
+import { InformationSectionProps } from "@/app/type/infomation";
 import { SectionContent } from "./sectionContent";
 import { SectionTitle } from "./title";
+import { BoardTitle } from "../../concept/atoms/title";
 
-export type Information = {
-  ADDRESS: {
-    TITLE: string;
-    VALUE: string;
-  };
-  ACCESS: {
-    TITLE: string;
-    VALUE: string;
-  };
-  STATION: {
-    TITLE: string;
-    STATION1: string;
-    STATION2: string;
-  };
-  PARKING: {
-    TITLE: string;
-    VALUE: string;
-  };
-  BUSINESS_DAY: {
-    TITLE: string;
-    TIME: string;
-    HOLIDAY: string;
-  };
-};
-
-type InformationSectionProps = {
-  information: Information;
-};
-
+/**
+ * インフォメーションコンポーネント
+ * @returns
+ */
 export const InformationSection = ({
   information,
 }: InformationSectionProps) => (
   <div className="bg-gray-100 p-4 md:p-8">
     <div className="bg-white p-4 rounded-lg shadow-md max-w-xl mx-auto">
-      {Object.entries(information).map(([key, section]) => (
-        <div key={key} className="my-4">
-          <SectionTitle text={section.TITLE} />
-          <SectionContent content={section} />
-        </div>
-      ))}
+      <BoardTitle title={"Information"} />
+      <div className="bg-white p-4 rounded-lg shadow-md">
+        {Object.entries(information).map(([key, section]) => (
+          <div key={key} className="my-4">
+            <SectionTitle text={section.TITLE} />
+            <SectionContent content={section} />
+          </div>
+        ))}
+      </div>
     </div>
   </div>
 );
