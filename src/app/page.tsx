80b1c2b3c8c046ed8632f22371dbcd8f9cc793af
watanabe0055 <mailok1212+github@gmail.com>
2023-08-20 16:13:38 +0900

    :memo: インフォメーションを定数ファイルのおいた

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Board from "./components/concept/bord";
+import Information from "./components/information";
 
 export default function Home() {
   return (
     <>
       <Board />
+      <Information />
     </>
   );
 }
