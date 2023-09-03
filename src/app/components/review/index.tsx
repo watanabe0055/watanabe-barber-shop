d55b737f0630151bbf67e528c9512c02b94564ea
watanabe0055 <mailok1212+github@gmail.com>
2023-09-03 23:52:39 +0900

    fix: 一旦修正

--- src/app/components/review/index.tsx
+++ src/app/components/review/index.tsx
@@ -1,27 +1,31 @@
 import { DUMMY_REVIEWS } from "@/app/const/review";
 import React from "react";
-import ReviewText from "../menu/atoms/review/reverText";
+
 import UserInfo from "../menu/atoms/review/userInfo";
+import ReviewText from "../menu/atoms/review/review";
+import ReviewTitle from "../menu/atoms/review/reviewTitle";
+import StarRating from "../menu/atoms/review/starRating";
 
 const Review = () => {
   return (
     <div className="bg-gray-100 p-4 md:p-8 text-gray-900">
-      {DUMMY_REVIEWS.map((review, index) => (
-        <div key={index} className="border p-6 m-3 rounded-md space-y-4">
-          <div className="pb-4">
-            <h2 className="text-2xl font-semibold">{review.TITLE}</h2>
-
-            <div className="text-xl">
-              {"★".repeat(review.RATING)}
-              {"☆".repeat(5 - review.RATING)}
-            </div>
-            <div className="flex justify-between text-sm">
-              <ReviewText text={review.REVIEW_TEXT} />
-              <UserInfo userName={review.USER_NAME} date={review.DATA} />
+      <div className="flex flex-wrap justify-between">
+        {DUMMY_REVIEWS.map((review, index) => (
+          <div
+            key={index}
+            className="border p-6 m-3 rounded-md space-y-4 w-full"
+          >
+            <div className="pb-4">
+              <ReviewTitle title={review.TITLE} />
+              <StarRating rating={review.RATING} />
+              <div className="flex justify-between text-sm">
+                <ReviewText text={review.REVIEW_TEXT} />
+                <UserInfo userName={review.USER_NAME} date={review.DATA} />
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
--- src/app/const/review/index.ts
+++ src/app/const/review/index.ts
@@ -14,4 +14,32 @@ export const DUMMY_REVIEWS = [
     USER_NAME: "Jane Doe",
     DATA: "2023-08-30",
   },
+  {
+    TITLE: "素晴らしい商品！",
+    RATING: 5,
+    REVIEW_TEXT: "この商品は本当に素晴らしいです。おすすめです！",
+    USER_NAME: "John Doe",
+    DATA: "2023-09-03",
+  },
+  {
+    TITLE: "まあまあです",
+    RATING: 3,
+    REVIEW_TEXT: "悪くはないですが、改善の余地あり。",
+    USER_NAME: "Jane Doe",
+    DATA: "2023-08-30",
+  },
+  {
+    TITLE: "素晴らしい商品！",
+    RATING: 5,
+    REVIEW_TEXT: "この商品は本当に素晴らしいです。おすすめです！",
+    USER_NAME: "John Doe",
+    DATA: "2023-09-03",
+  },
+  {
+    TITLE: "まあまあです",
+    RATING: 3,
+    REVIEW_TEXT: "悪くはないですが、改善の余地あり。",
+    USER_NAME: "Jane Doe",
+    DATA: "2023-08-30",
+  },
 ];
