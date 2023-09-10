223c292eb00b9cc994ff613052590dba7de30d5f
watanabe0055 <mailok1212+github@gmail.com>
2023-09-10 17:39:55 +0900

    fix: 少しスタイル種性

--- src/app/components/news/index.tsx
+++ src/app/components/news/index.tsx
@@ -1,15 +1,24 @@
-import { News } from "@/app/libs/client";
+import { getList } from "@/app/libs/client";
 import { NewsHeadLine } from "./newsHeadLline";
 
-type NewsProps = {
-  news: News;
-};
-const NewsList = ({ news }: NewsProps) => {
+const NewsListSection = async () => {
+  const { contents } = await getList();
+
+  if (!contents || contents.length === 0) {
+    return <h1>No contents</h1>;
+  }
   return (
     <>
-      <NewsHeadLine news={news} />
+      <section className="board bg-gray-100 p-4 md:p-8 text-gray-900">
+        <div className="bg-white p-4 rounded-lg shadow-md max-w-xl mx-auto">
+          <h3 className="p-5 text-2xl font-bold text-center">Menu</h3>
+          {contents.map((news) => (
+            <NewsHeadLine key={news.id} news={news} />
+          ))}
+        </div>
+      </section>
     </>
   );
 };
 
-export default NewsList;
+export default NewsListSection;
--- src/app/static/page.tsx
+++ src/app/static/page.tsx
@@ -1,33 +1,17 @@
 import Board from "../components/concept";
 import Information from "../components/information";
 import Menu from "../components/menu";
-import NewsList from "../components/news";
+import NewsListSection from "../components/news";
 import Review from "../components/review";
-import { getList } from "../libs/client";
 
 export default async function StaticPage() {
-  const { contents } = await getList();
-  console.log("StaticPage:", contents);
-
-  if (!contents || contents.length === 0) {
-    return <h1>No contents</h1>;
-  }
-
   return (
     <>
       <Board />
       <Information />
       <Menu />
       <Review />
-      <ul>
-        {contents.map((post) => {
-          return (
-            <div key={post.id}>
-              <NewsList news={post} />
-            </div>
-          );
-        })}
-      </ul>
+      <NewsListSection />
     </>
   );
 }
