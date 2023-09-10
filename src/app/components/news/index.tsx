dc972be4399e682e5ddec55ecdcb1a833b61d4ed
watanabe0055 <mailok1212+github@gmail.com>
2023-09-11 00:18:10 +0900

    :sparkles:newsアトムのスタイル修正

--- src/app/components/news/index.tsx
+++ src/app/components/news/index.tsx
@@ -11,10 +11,14 @@ const NewsListSection = async () => {
     <>
       <section className="board bg-gray-100 p-4 md:p-8 text-gray-900">
         <div className="bg-white p-4 rounded-lg shadow-md max-w-xl mx-auto">
-          <h3 className="p-5 text-2xl font-bold text-center">Menu</h3>
-          {contents.map((news) => (
-            <NewsHeadLine key={news.id} news={news} />
-          ))}
+          <h3 className="p-5 text-2xl font-bold text-center border-b border-gray-200 mb-4">
+            Menu
+          </h3>
+          <div className="space-y-4">
+            {contents.map((news) => (
+              <NewsHeadLine key={news.id} news={news} />
+            ))}
+          </div>
         </div>
       </section>
     </>
--- src/app/components/news/newsHeadLline/index.tsx
+++ src/app/components/news/newsHeadLline/index.tsx
@@ -9,10 +9,10 @@ export const NewsHeadLine = ({ news }: NewsProps) => {
   const publishedAt = dayjs(news.createdAt).format("YYYY年MM月DD日");
   return (
     <>
-      <div>
-        <p>{publishedAt}</p>
-        <h3>タイトル:{news.title}</h3>
-        <h4>{news.description}</h4>
+      <div className="p-4 bg-gray-50 rounded-lg shadow hover:shadow-lg transition-shadow">
+        <p className="text-sm text-gray-600">{publishedAt}</p>
+        <h3 className="text-xl font-semibold my-2">タイトル: {news.title}</h3>
+        <h4 className="text-lg text-gray-800">{news.description}</h4>
       </div>
     </>
   );
