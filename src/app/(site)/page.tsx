e0418fba86c15b798ff78e4ceca990da4a8618f1
watanabe0055 <mailok1212+github@gmail.com>
2023-09-16 22:01:38 +0900

    fat: pagesディレクトリ配下でも/で見れるようになった

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,15 +1,19 @@
 import Board from "../components/concept";
 import Information from "../components/information";
 import Menu from "../components/menu";
+import NewsListSection from "../components/news";
 import Review from "../components/review";
 
-export default function Home() {
+export const Pages = () => {
   return (
     <>
       <Board />
       <Information />
       <Menu />
       <Review />
+      <NewsListSection />
     </>
   );
-}
+};
+
+export default Pages;
