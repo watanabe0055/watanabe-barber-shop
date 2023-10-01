af4fb4ab2a678a93a29daf64c316f09cb81e6443
watanabe0055 <mailok1212+github@gmail.com>
2023-10-02 02:48:21 +0900

    feat: モーダルを表示するとこまで実装

--- src/app/components/menu/atoms/menu/menuAllList/index.tsx
+++ src/app/components/menu/atoms/menu/menuAllList/index.tsx
@@ -1,25 +1,49 @@
+"use client";
 import { MenuType } from "@/app/type/menu";
 import MenuCategory from "../menuCategory";
 import PriceData from "../../priceData";
 import Typography from "@/app/atoms/Typography";
+import { useState } from "react";
+import Modal from "react-modal";
 
 interface MenuListProps {
   menuList: MenuType;
 }
 
 const MenuAllList: React.FC<MenuListProps> = ({ menuList }) => {
+  const [modalIsOpen, setIsOpen] = useState(false);
+  Modal.setAppElement("#modal_element");
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
   return (
     <>
       <Typography text="Menu" size="xl3" weight="bold" align="center" />
       {Object.keys(menuList).map((listName) => {
         const key = listName as keyof MenuType;
         return (
-          <div key={listName}>
+          <div key={listName} id="modal_element">
             <MenuCategory listName={listName} />
             <ul className="pl-3">
               {menuList[key].map((item, index) => (
                 <li key={index}>
-                  <PriceData item={item} />
+                  <button onClick={openModal}>
+                    <PriceData item={item} />
+                  </button>
+                  <Modal
+                    isOpen={modalIsOpen}
+                    onRequestClose={closeModal}
+                    contentLabel="Example Modal"
+                  >
+                    <button onClick={closeModal}>閉じる</button>
+                    <div>
+                      <PriceData item={item} />
+                    </div>
+                  </Modal>
                 </li>
               ))}
             </ul>
